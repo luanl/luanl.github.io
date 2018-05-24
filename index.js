82221ec46dd958e0113177694624d2e5ce756bfc
luanl <luan.lop22@gmail.com>
2018-05-23 23:55:07 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+          document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.parallax');
+            var instances = M.Parallax.init(elems, options);
+          });
+
+          // Or with jQuery
+
+          $(document).ready(function(){
+            $('.parallax').parallax();
+            var instance = M.Parallax.getInstance(elem);
+          });
